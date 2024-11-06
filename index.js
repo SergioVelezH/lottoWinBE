55770f8d62c9a19e49c04692a526b228e42f176c
Sergio <sergiovelezhernandez11@gmail.com>
2024-11-06 12:17:02 -0300

    usdt

--- index.js
+++ index.js
@@ -8,13 +8,19 @@ const createDefaultGames = async () => {
     const existingGame = await Game.findOne();
 
     if (!existingGame) {
+
+      const prizesUsdt = await Prize.findAll();
+
+      // Calcula la sumatoria del campo 'usdt' de todos los premios
+      const totalUsdt = prizesUsdt.reduce((sum, prize) => sum + Number(prize.usdt), 0);
+
       const games = await Game.bulkCreate([
         {
           name: "IPHONE 14 PRO MAX",
           nft: 5000,
           image:
             "https://s3-alpha-sig.figma.com/img/9817/b2be/22af5934b9d8ff54c6037690b479df6d?Expires=1730678400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=KXEm58kt8NIjQlyJOV9XEomU~myApTBhCzNIdl-fdL5sm2aJJYUdB87QEg1wgGvkoXASJUCfQgRrTh6v7emyDOcP3Ya5paLIA-7TVp~iWmdgkHGBCBHVXpFwogGP3KFcJdJkQtWkTHoNtyw2HfYDNJdl1C2VBsLg0wf450a2VEw1UDzDVgwjNleFjs9NwW~S8tYR6QlysKNUKdjeJYPwNU3~rUz9JcaQO9-xfLSzcyg6XXh7t4YkoaedKku~9dQ9GCdsmEDuBZA9ByF-IMU5GGeCD7HchKUIxw-1Vaur5ySp~hkPewLk7yE5jiJ8qr5Ove6~TOZyKKYcmeaZn8sx0A__",
-          usdt: "100.000",
+          usdt: totalUsdt,
           size: "MEDIUM",
         },
         {
@@ -22,7 +28,7 @@ const createDefaultGames = async () => {
           nft: 1000,
           image:
             "https://s3-alpha-sig.figma.com/img/9446/f851/7f8242c71755fec0a2ce1687bfe5c52e?Expires=1730678400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=nNpolJTZm3Y~tOfwNOzruItRkNEeXnCBOCRK-SV0plNEjntvJ3nliR0u66HGaP-Gm50ZwVfEZqvGMaC2cfA6S-XMXOr-YgHEGUdxHfM7MARzPh~CPhOCWLpd4ABw9Nw7gUCTVV1DCWXdVrt5Xb~Hfub7qQksSEHIvdqGf8p7wopE87RFNOTmnKybqpezn3DAChpY2Pfp0T~OudLpjkmiVrgPNXDgIVNTtnSbYdoNuchG5issW4PMv55CIhpBx2ba8xI599UwDWEkyUXR-CvHmG8Yzz7rFEoRT-O~KcUK3XRT0rEZ~RP4eBqfEHDMJdskn8VIfXTcci3Yxw41VKtasA__",
-          usdt: "340.000",
+          usdt: totalUsdt,
           size: "BIG",
         },
         {
@@ -30,7 +36,7 @@ const createDefaultGames = async () => {
           nft: 2000,
           image:
             "https://s3-alpha-sig.figma.com/img/ac13/92fc/6d8671ca2517bb88873d2cc76d94ed96?Expires=1730678400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=DfGV4IC2sVp8tMvO~PBZydWVxfz7NciG6begMfZD7oVIaf4zEDB4mN67JLxDgyL1qCAaWFdVZTwbDmd70orUzcdzQvfV~TWfJJHptB4QSKVyCKrwn033q5iOnqlKAsFXNlZUkaOEVeInP7Ft5K7s~Ahc3pslEiOlZUYr~XROzmmWCRnUcu-ssSryHdB8xaO1l1hTryq6mzYUb6bi3JFvfGNbi4Oq6lrbB8v5egQI0F6aMLc9Sq7ZZ-sj2eLnZsYcFsuHocq1xfYyZEoVUrkeOt4TPFQclciSCJimpFoa9u-h7aCbGzPgoI-v0NdclWqyhCyGRfoLifR4jK2QZD~HZg__",
-          usdt: "30.000",
+          usdt: totalUsdt,
           size: "MEDIUM",
         },
         {
@@ -38,7 +44,7 @@ const createDefaultGames = async () => {
           nft: 3000,
           image:
             "https://s3-alpha-sig.figma.com/img/30dd/b36b/477d39c46bb58bd7cf2b527fe696e621?Expires=1730678400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=UXPDfiyPAzLUHzilPfOb7jKoGeaiH3TCcn4-oZ3EKEepKotWem14qJVZEo2tMBzzPK0m-DKVXek7xTRYOYpq252IQpxyhqAQF2VL4QSORBSqBgAFHTlB6TbBYdxYde83xtQcwm7Ma61UsD-RCfoJKNavxeeSDQ3oQBAA~8cDQaPwwWxDBN9i18UnjctFuKftZD~MQMI6Zi6WkwSu5WBl9IqIS5MlVrXA-WWbc7GMDyjSdKIYLNSO43v2ef9RrNbpm5NkcTSmuYlDShTlANRv7GvmpZuwgsCF5iddXUZZVIkY96fu9LYc61X0boRwM-oz8UuoF-ilDvwsrJYYfbmBVw__",
-          usdt: "140.000",
+          usdt: totalUsdt,
           size: "MEDIUM",
         },
       ]);
@@ -74,7 +80,7 @@ const createDefaultPrizes = async () => {
           name: "TESLA MODEL S",
           stock: "1",
           image:
-            "https://s3-alpha-sig.figma.com/img/9446/f851/7f8242c71755fec0a2ce1687bfe5c52e?Expires=1730678400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=nNpolJTZm3Y~tOfwNOzruItRkNEeXnCBOCRK-SV0plNEjntvJ3nliR0u66HGaP-Gm50ZwVfEZqvGMaC2cfA6S-XMXOr-YgHEGUdxHfM7MARzPh~CPhOCWLpd4ABw9Nw7gUCTVV1DCWXdVrt5Xb~Hfub7qQksSEHIvdqGf8p7wopE87RFNOTmnKybqpezn3DAChpY2Pfp0T~OudLpjkmiVrgPNXDgIVNTtnSbYdoNuchG5issW4PMv55CIhpBx2ba8xI599UwDWEkyUXR-CvHmG8Yzz7rFEoRT-O~KcUK3XRT0rEZ~RP4eBqfEHDMJdskn8VIfXTcci3Yxw41VKtasA__",
+            "https://s3-alpha-sig.figma.com/img/9446/f851/7f8242c71755fec0a2ce1687bfe5c52e?Expires=1731888000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Kgyrab8DVMKzJAM-yJsr-K4naCf5mqpmT3dZiSgiec8uSLDrHWmqUl2oEKnld0mvqvXjbwQHndjgczK8A7nTKoUhuo2jKvF8Td-dyPEVkhiJ7Cbv5RQ80MxShNhIwP67bva7S5phgCUxlSEpsFfGEILDQg-KDlB5kZt5vUbV33K5BOe2xMJ6b~J4icqmEwv-HuxpiVk9D9OB-X2xIlSnnPAxe4eRWI4cgrurEPEnCN~ERLsWEBH5jFhCGKfi~x5s0qDU0C5s5rfqSR6pGbASPEAe41xvPHSJSteDpRnlSzads8KpjlgA70iTq4dIbMhIl~4zzGpk0Oj6VuKLkjemZw__",
           usdt: "200.000",
         },
         {
--- src/controllers/gameControllers.js
+++ src/controllers/gameControllers.js
@@ -10,24 +10,27 @@ const getAllGames = async () => {
   return games;
 };
 
-const createGame = async ({ name, image, nft, usdt, size }) => {
+const createGame = async ({ name, image, nft, size }) => {
   try {
+    // Obtén todos los premios
     const prizes = await Prize.findAll();
-
+    // Calcula la sumatoria del campo 'usdt' de todos los premios
+    const totalUsdt = prizes.reduce((sum, prize) => sum + prize.usdt, 0);
+    // Crea el juego con la sumatoria de 'usdt' de los premios
     const newGame = await Game.create({
       name,
       image,
       nft,
-      usdt,
+      usdt: totalUsdt, // Asigna la sumatoria al campo usdt del juego
       size,
     });
-
+    // Asocia el juego con los premios
     await newGame.addPrizes(prizes);
 
     return newGame;
   } catch (error) {
     console.error("Error al crear el juego:", error);
-    throw error; // Propaga el error para que pueda ser manejado por el llamador
+    throw error;
   }
 };
 
